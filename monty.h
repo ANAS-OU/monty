110ae7b39f181c15a045b8856b61c92c4c76ab45
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-12-14 17:24:22 +0100

    Header File

---
+++ monty.h
@@ -0,0 +1,60 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+/****************
+ * std Libraries
+ ****************/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#define LINESIZE 256
+
+/*******************
+ * Compix DataTypes
+ *******************/
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+
+/**********************
+ * Functions Prototype
+ **********************/
+void op_handler(char *filename);
+void (*get_op_func(char *opcode))(stack_t **stack, unsigned int line_number);
+void push(stack_t **top, unsigned int line_number);
+void pall(stack_t **top, unsigned int line_number);
+char **str_split(char *str, char *by);
+unsigned int token_count(char *str, char *by);
+void print_err(int status, ...);
+int _isdigit(char *str);
+
+#endif /* MONTY_H */
