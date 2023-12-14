a4d2d66d3ced88bf662a4bcd4437274c52201209
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-12-14 17:26:33 +0100

    function the that return pointer to operators

---
+++ get_op_func.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * get_op_func - function that returns a pointer
+ * to the function that corresponds to the operator
+ * @opcode: a given opcode.
+ *
+ * Return: operation result.
+ */
+
+void (*get_op_func(char *opcode))(stack_t **stack, unsigned int line_number)
+{
+	instruction_t ops[] = {
+		{"push", push},
+		{"pall", pall},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].opcode)
+	{
+		if (!strcmp(ops[i].opcode, opcode))
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
