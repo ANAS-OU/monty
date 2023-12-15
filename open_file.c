151cb680d6ab9cc76b180bbf695c36276076433c
Anass Outiskte <anas.outiskte@edu.uiz.ac.ma>
2023-12-16 00:36:11 +0100

    handling comments

--- open_file.c
+++ open_file.c
@@ -29,7 +29,7 @@ void open_file(char *filename)
 		opcode = strtok(buffer, delim);
 		data = strtok(NULL, delim);
 
-		if (!opcode)
+		if (!opcode || buffer[0] == '#')
 			continue;
 
 		op_handler(opcode, data, line_number);
